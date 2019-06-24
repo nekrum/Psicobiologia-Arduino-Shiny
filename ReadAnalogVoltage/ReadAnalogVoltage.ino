227654de67431c85b01aa43991375bc947d57567
nekrum <nekrum@gmail.com>
2019-06-24 15:09:36 -0500

    Remueve comentarios, y agrega delay para captura cada segundo.

--- ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -1,52 +1,31 @@
-#include <SPI.h>
-#include <SD.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
- 
-// Pin donde se conecta el bus 1-Wire
-const int pinDatosDQ = 7;
+#include <SPI.h>
+#include <SD.h>
+#define ONE_WIRE_BUS 2 
 
-// Instancia a las clases OneWire y DallasTemperature
-OneWire oneWireObjeto(pinDatosDQ);
-DallasTemperature sensorDS18B20(&oneWireObjeto);
-
-// the setup routine runs once when you press reset:
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+float temp=0;
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   while (!Serial) {
-              ; // wait for serial port to connect. Needed for native USB port only
+              ;
   }
-  sensorDS18B20.begin(); 
-          
-  Serial.print("Initializing SD card... ");
-  if (!SD.begin(9)) {
-    Serial.println("Initialization failed");
-    return;
-  }
-  Serial.println("Initialization done");     
+  sensors.begin();
 }
 
 
 void loop() {
   String dataString = "";
-  sensorDS18B20.requestTemperatures();
+  sensors.requestTemperatures();
   int sensorValue = analogRead(A0);
-  dataString += "V: ";
-  dataString += String(sensorValue);
-  dataString += ",";
-  dataString += "C: ";
   float voltage = sensorValue * (5.0 / 1023.0);
-  dataString += String(sensorValue);
+  float temp = (sensors.getTempCByIndex(0));
+  dataString += "V: ";
+  dataString += String(voltage);
+  dataString += ", C: ";
+  dataString += String(temp);
   Serial.println(dataString);
-  SD.open("datalog.txt", FILE_WRITE);
-  if (myFile) {
-    Serial.print("Writing to testsd.txt...");
-    myFile.println(dataString);
-    myFile.close();
-    Serial.println("Done");
-   } else {
-    // If the file didn't open, print an error:
-    Serial.println("Error opening testsd.txt");
-   }
+  delay(1000);
 }
